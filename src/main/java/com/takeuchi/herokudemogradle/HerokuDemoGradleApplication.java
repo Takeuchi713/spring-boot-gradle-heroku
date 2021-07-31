ffe713cce78da2f2295029201c3b777601ce2beb
Takeuchi713 <52090424+Takeuchi713@users.noreply.github.com>
2021-07-31 15:12:35 +0900

    first commit

---
+++ src/main/java/com/takeuchi/herokudemogradle/HerokuDemoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.takeuchi.herokudemogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDemoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDemoGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takeuchi/herokudemogradle/HerokuDemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeuchi.herokudemogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
